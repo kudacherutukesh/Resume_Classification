f2a92cce15e91c1cbc54381b0249cdc64a326ff9
Rutukesh Chandrakant Kudache <kudacherutukesh@gmail.com>
2026-05-21 01:06:27 +0530

    Added project files

---
+++ streamlit_app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:556817b104b7e23a0a8d4fd89b18463695e741ab7f899915ed7c6d7ee062d3e9
+size 2781
---
+++ train_model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1796690dface229a759c77315d4aaaabe8ba7a3f95be5a31c70c610606801e7f
+size 2006
